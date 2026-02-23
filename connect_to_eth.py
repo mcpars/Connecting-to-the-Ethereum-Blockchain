9625d57ed4b8e21c88c4ea84ea12c387870a00cd
mcpars <157234335+mcpars@users.noreply.github.com>
2026-02-22 20:01:34 -0500

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -28,7 +28,7 @@ def connect_with_middleware(contract_json):
 
 
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	bob_url = "https://data-seed-prebsc-1-s1.binance.org:8545/""
+	bob_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
   
   w3 = Web3(HTTPProvider(bnb_url))
   assert w3, is_connected(), f"Failed to connect: {bnb_url}"
